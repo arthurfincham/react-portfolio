85940f548904b09ed3c62e707c31bcfc5d7a5037
Arthur Fincham <arthurfincham@me.com>
2021-12-02 23:01:51 +0000

    all working, add github text svg for later use

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import plant from './svgs/nounPlant.svg';
 import gitLogo from './svgs/githubLogo.svg';
 import linkLogo from './svgs/linkedinLogo.svg';
+import gitText from './svgs/githubText.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <a className="logo-link" href="https://github.com/arthurfincham">
+        <a className="logo-link" target="blank" href="https://github.com/arthurfincham">
           <img src={gitLogo} className="giHu-icon" alt="logo" />
         </a>
-        <a className="logo-link" href="https://www.linkedin.com/in/arthurfincham/">
+        <a className="logo-link" target="blank" href="https://www.linkedin.com/in/arthurfincham/">
           <img src={linkLogo} className="liIn-icon" alt="logo" />
         </a>
+        <img src={gitText} class="logo-link" alt="text" />
       </header>
       <div className="Arthur-plant">
         <div className="Welcome-text">
