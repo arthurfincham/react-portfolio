4f072b20bf80fa55c0e08f89a158f9092a687971
Arthur Fincham <arthurfincham@me.com>
2021-12-03 11:06:35 +0000

    sticky header is working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import plant from './svgs/nounPlant.svg';
 import AnchorLinks from './components/AnchorLinks';
+import Education from './components/Education';
 import plusLine from './svgs/plusDivider.svg';
 import IconLinks from './components/IconLinks';
 import './App.css';
@@ -17,29 +18,7 @@ function App() {
         <AnchorLinks />
       </div>
       <img src={plusLine} className="Plus-line" alt="logo" />
-      <div class="section-box" id="Education">
-        <h1>Education</h1>
-        <h2>Makers Academy</h2>
-        <h5 class="">2021</h5>
-        <p class="">16 week highly selective development bootcamp;</p>
-        <ul>
-          <li class="">OOP, TDD, MVC, DDD</li>
-          <li class="">Agile/XP</li>
-          <li class="">Ruby, Rails, JavaScript</li>
-          <li class="">RSpec, Jasmine</li>
-        </ul>
-        <hr class="" />
-        <h2>King's College London</h2>
-        <h4>BA Philosophy</h4>
-        <h5 class="">2018-2021</h5>
-        <p class="">First Class Honours</p>
-        <ul>
-          <li class="">Thesis on The Consequences of Determinism for Moral Responsibility.</li>
-          <li class="">Modules in Elementary Logic & Philosophical Logic</li>
-          <li class="">Half of my degree was spent at home due to Covid, which threw me into the deep end of working remotely!</li>
-        </ul>
-        <hr class="" />
-      </div>
+      <Education />
     </div>
   );
 }
---
+++ src/components/Education.js
@@ -0,0 +1,52 @@
+export default function Education() {
+  return (
+    // <div class="section-box" id="Education">
+    //   <h1>Education</h1>
+    //   <h2>Makers Academy</h2>
+    //   <h5 class="">2021</h5>
+    //   <p class="">16 week highly selective development bootcamp;</p>
+    //   <ul>
+    //     <li class="">OOP, TDD, MVC, DDD</li>
+    //     <li class="">Agile/XP</li>
+    //     <li class="">Ruby, Rails, JavaScript</li>
+    //     <li class="">RSpec, Jasmine</li>
+    //   </ul>
+    //   <hr class="" />
+    //   <h2>King's College London</h2>
+    //   <h4>BA Philosophy</h4>
+    //   <h5 class="">2018-2021</h5>
+    //   <p class="">First Class Honours</p>
+    //   <ul>
+    //     <li class="">Thesis on The Consequences of Determinism for Moral Responsibility.</li>
+    //     <li class="">Modules in Elementary Logic & Philosophical Logic</li>
+    //     <li class="">Half of my degree was spent at home due to Covid, which threw me into the deep end of working remotely!</li>
+    //   </ul>
+    //   <hr class="" />
+    // </div>
+    <div className="section-box" id="Education">
+      <table className="sticky-thc">
+        <thead>
+          <tr>
+            <th>Education</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="body-row">
+            <td>First</td>
+            <td>First</td>
+          </tr>
+          <tr className="body-row">
+            <td>Second</td>
+            <td>Second</td>
+            <td>Second</td>
+          </tr>
+          <tr className="body-row">
+            <td>Third</td>
+            <td>Third</td>
+            <td>Third</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
