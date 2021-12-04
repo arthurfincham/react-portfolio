8bd171cafcdab127cf6759d5298ab990e95ec7cb
Arthur Fincham <arthurfincham@me.com>
2021-12-04 22:14:12 +0000

    update horizontal

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,6 +1,7 @@
 import UniIcon from './graphics/UniIcon';
 import Divider from './graphics/Divider';
 import TechHead from './graphics/TechHead';
+import Fade from 'react-reveal/Fade';
 
 export default function Education() {
   const fill = 'white';
@@ -11,40 +12,50 @@ export default function Education() {
       <div className="item-container">
         <div className="flex-row ">
           <div>
-            <h2>Makers Academy</h2>
+            <Fade cascade>
+              <h2>Makers Academy</h2>
+            </Fade>
             <div className="flex-col">
-              <h4>Software Engineering</h4>
-              <h3>2021</h3>
+              <Fade cascade>
+                <h4>Software Engineering</h4>
+                <h3>2021</h3>
+              </Fade>
             </div>
-            <p>16 week highly selective development bootcamp;</p>
-            <ul>
-              <li>OOP, TDD, MVC, DDD</li>
-              <li>Agile/XP</li>
-              <li>Ruby, Rails, JavaScript</li>
-              <li>RSpec, Jasmine</li>
-            </ul>
+            <Fade cascade>
+              <p>16 week highly selective development bootcamp;</p>
+            </Fade>
+            <Fade cascade>
+              <ul>
+                <li>OOP, TDD, MVC, DDD</li>
+                <li>Agile/XP</li>
+                <li>Ruby, Rails, JavaScript</li>
+                <li>RSpec, Jasmine</li>
+              </ul>
+            </Fade>
           </div>
           <TechHead fill={fill} svgClass={'decorative'} svgId="tech-head" />
         </div>
       </div>
       <Divider fill={fill} />
       <div className="item-container">
-        <div className="flex-row-rev ">
-          <div className="w-45ch">
-            <h2>King's College London</h2>
-            <div className="flex-col-right">
-              <h4>BA Philosophy</h4>
-              <h5>2018-2021</h5>
-              <p>First Class Honours</p>
+        <Fade cascade>
+          <div className="flex-row-rev ">
+            <div className="w-45ch">
+              <h2>King's College London</h2>
+              <div className="flex-col-right">
+                <h4>BA Philosophy</h4>
+                <h5>2018-2021</h5>
+                <p>First Class Honours</p>
+              </div>
+              <ul>
+                <li>Thesis on The Consequences of Determinism for Moral Responsibility.</li>
+                <li>Modules in Elementary Logic & Philosophical Logic</li>
+                <li>Half of my degree was spent at home due to Covid, which threw me into the deep end of working remotely!</li>
+              </ul>
             </div>
-            <ul>
-              <li>Thesis on The Consequences of Determinism for Moral Responsibility.</li>
-              <li>Modules in Elementary Logic & Philosophical Logic</li>
-              <li>Half of my degree was spent at home due to Covid, which threw me into the deep end of working remotely!</li>
-            </ul>
+            <UniIcon fill={fill} svgClass={'decorative'} svgId="uni-icon" />
           </div>
-          <UniIcon fill={fill} svgClass={'decorative'} svgId="uni-icon" />
-        </div>
+        </Fade>
       </div>
     </div>
   );
