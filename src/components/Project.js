de380530d7358208a9378158bc61356f57797c8c
Arthur Fincham <arthurfincham@me.com>
2021-12-04 16:28:54 +0000

    add previews

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,21 +1,24 @@
 import Divider from './graphics/Divider';
 import GitHubLogo from './graphics/GitHubLogo.js';
 
-export default function Project({ props }) {
+export default function Project({ props, iconData, rowOrder }) {
   const svgFill = 'white';
   const svgClass = 'project-repo-link svg-link';
   return (
     <>
       <Divider fill={svgFill} />
       <div className="item-container">
-        <div className="flex-row row-center">
-          <h2>{props.title}</h2>
-          <a href={props.repoURL}>
-            <GitHubLogo fill={svgFill} svgClass={svgClass} />
-          </a>
+        <div className={rowOrder}>
+          <div className="flex-col w-45ch">
+            <a href={props.repoURL}>
+              <GitHubLogo fill={svgFill} svgClass={svgClass} />
+            </a>
+            <h2>{props.title}</h2>
+            <h5>{props.subTitle}</h5>
+            <p>{props.description}</p>
+          </div>
+          {iconData}
         </div>
-        <h5>{props.subTitle}</h5>
-        <p>{props.description}</p>
       </div>
     </>
   );
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,14 +1,17 @@
 import Project from './Project';
+import acebookPreview from './graphics/acebookPreview.svg';
+import instaclonePreview from './graphics/instaclonePreview.svg';
+
 import { classNotes, acebook, instaclone, summary, chitter } from '../data/ProjectData';
 export default function Projects() {
   return (
     <div className="section-box" id="Projects">
       <h1 className="sticky-thc">Projects</h1>
-      <Project props={classNotes} />
-      <Project props={instaclone} />
-      <Project props={acebook} />
-      <Project props={summary} />
-      <Project props={chitter} />
+      <Project props={classNotes} rowOrder={'flex-row'} iconData={'asd'} />
+      <Project props={instaclone} rowOrder={'flex-row-rev'} iconData={<img src={instaclonePreview} className="instaclone-preview" />} />
+      <Project props={acebook} rowOrder={'flex-row'} iconData={<img src={acebookPreview} className="acebook-preview" />} />
+      <Project props={summary} rowOrder={'flex-row-rev'} iconData={'asd'} />
+      <Project props={chitter} rowOrder={'flex-row'} iconData={'asd'} />
     </div>
   );
 }
