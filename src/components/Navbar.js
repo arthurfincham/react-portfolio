efede50a641723b5c84292afef7eae247f2bd6f2
Arthur Fincham <arthurfincham@me.com>
2021-12-10 16:42:52 +0000

    update desktop nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,33 +1,103 @@
 import React from 'react';
 import IconLinks from './IconLinks';
-import Fade from 'react-reveal/Fade';
 
 export default function Navbar({ section1InView, section2InView, section3InView, section4InView, section5InView }) {
   let determineTitle = () => {
     if (section1InView) {
-      return '';
+      return (
+        <>
+          <a className="anchor" href="#Education">
+            <h3 className="navTitle">education</h3>
+          </a>
+          <a className="anchor" href="#Experience">
+            <h3 className="navTitle">experience</h3>
+          </a>
+          <a className="anchor" href="#Projects">
+            <h3 className="navTitle">projects</h3>
+          </a>
+          <a className="anchor" href="#Pontact">
+            <h3 className="navTitle">contact</h3>
+          </a>
+        </>
+      );
     }
     if (section2InView) {
-      return 'Education';
+      return (
+        <>
+          <a className="anchor" href="#Education">
+            <h3 className="navTitle-active">education</h3>
+          </a>
+          <a className="anchor" href="#Experience">
+            <h3 className="navTitle">experience</h3>
+          </a>
+          <a className="anchor" href="#Projects">
+            <h3 className="navTitle">projects</h3>
+          </a>
+          <a className="anchor" href="#Contact">
+            <h3 className="navTitle">contact</h3>
+          </a>
+        </>
+      );
     }
     if (section3InView) {
-      return 'Experience';
+      return (
+        <>
+          <a className="anchor" href="#Education">
+            <h3 className="navTitle">education</h3>
+          </a>
+          <a className="anchor" href="#Experience">
+            <h3 className="navTitle-active">experience</h3>
+          </a>
+          <a className="anchor" href="#Projects">
+            <h3 className="navTitle">projects</h3>
+          </a>
+          <a className="anchor" href="#Contact">
+            <h3 className="navTitle">contact</h3>
+          </a>
+        </>
+      );
     }
     if (section4InView) {
-      return 'Projects';
+      return (
+        <>
+          <a className="anchor" href="#Education">
+            <h3 className="navTitle">education</h3>
+          </a>
+          <a className="anchor" href="#Experience">
+            <h3 className="navTitle">experience</h3>
+          </a>
+          <a className="anchor" href="#Projects">
+            <h3 className="navTitle-active">projects</h3>
+          </a>
+          <a className="anchor" href="#Contact">
+            <h3 className="navTitle">contact</h3>
+          </a>
+        </>
+      );
     }
     if (section5InView) {
-      return 'Contact';
+      return (
+        <>
+          <a className="anchor" href="#Education">
+            <h3 className="navTitle">education</h3>
+          </a>
+          <a className="anchor" href="#Experience">
+            <h3 className="navTitle">experience</h3>
+          </a>
+          <a className="anchor" href="#Projects">
+            <h3 className="navTitle">projects</h3>
+          </a>
+          <a className="anchor" href="#Contact">
+            <h3 className="navTitle-active">contact</h3>
+          </a>
+        </>
+      );
     }
   };
   let sectionTitle = determineTitle;
   return (
     <nav className="navbar">
-      <h2 className="navTitle">
-        <Fade duration={500} spy={sectionTitle()}>
-          {sectionTitle()}
-        </Fade>
-      </h2>
+      {sectionTitle()}
       <IconLinks />
     </nav>
   );
