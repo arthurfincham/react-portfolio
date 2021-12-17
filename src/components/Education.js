c14b6201c08e17914eaebc4c4b22886f781ce614
Arthur Fincham <arthurfincham@me.com>
2021-12-17 15:23:11 +0100

    update templates

--- src/components/Education.js
+++ src/components/Education.js
@@ -5,7 +5,7 @@ import TechHead from './graphics/TechHead';
 export default function Education({ section2Ref }) {
   const svgClass = 'svg-stroke svg';
   return (
-    <div className="section-box third-wrapper" id="Education" ref={section2Ref}>
+    <div className="section-box edu-wrapper" id="Education" ref={section2Ref}>
       <Divider />
       <div className="third-item">
         <div className="third-text">
@@ -22,6 +22,25 @@ export default function Education({ section2Ref }) {
       </div>
       <Divider />
       <div className="third-item">
+        <div className="third-description">
+          <p>
+            After teaching myself some basics of programming, I applied to Makers: a 16 week highly selective development bootcamp.
+            <br />
+            <br />
+            We began with <span className="text-darker">TDD</span> in <span className="text-darker">Ruby</span> and{' '}
+            <span className="text-darker">Rspec</span> before moving to <span className="text-darker">OOD</span>, databases and web development. By
+            the end of the course, I was confident and competent in building a full-stack application.
+            <br />
+            <br />
+            While I could have taught myself some of the programming skills, I felt that the experience of{' '}
+            <span className="text-darker">Pair Programming</span> and collaborating with other was invaluable.
+            <br />
+            <br />
+            Our cohort was hybrid, meaning that our second of three projects was done remotely. Using <span className="text-darker">Agile</span>{' '}
+            methodologies through tools like Trello and <span className="text-darker">GitHub</span>, we made good progress and got a taste of working
+            in the real world.
+          </p>
+        </div>
         <div className="third-text">
           <h1>Makers Academy</h1>
           <h3>Software Engineering</h3>
@@ -35,7 +54,6 @@ export default function Education({ section2Ref }) {
         <div className="third-svg">
           <TechHead svgClass={svgClass} />
         </div>
-        
       </div>
     </div>
   );
